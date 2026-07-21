92547b8757bb6e5312c4a9e6d4c541fe9ec92fbd
Anh Tu Do <anhtu.do1998@gmail.com>
2026-07-21 09:45:33 +0800

    Added absolute schdeuling determining when next beat should be played, and only delay what is needed

---
+++ absolute_sched.py
@@ -0,0 +1,28 @@
+# absolute.py
+import time
+import numpy as np
+import sounddevice as sd
+
+SAMPLING_FREQUENCY=44_100
+
+def cook_sound(frequency: float = 440.0, duration: float = 0.05):
+    num_samples = int(SAMPLING_FREQUENCY * duration)
+    t = np.arange(num_samples)/SAMPLING_FREQUENCY # convert to store time that the samples are taken
+    envelope = np.exp(-t*1000)
+    amplitude = 0.8
+    sound = (amplitude * envelope * np.sin(2*np.pi*frequency*t)).astype(np.float32)
+    return sound
+
+SOUND = cook_sound()
+BPM = 120
+interval = 60.0 / BPM
+
+t0 = time.perf_counter()
+n = 0
+while True:
+    sd.play(SOUND, SAMPLING_FREQUENCY)
+    n += 1
+    deadline = t0 + n * interval            # absolute, computed from t0
+    delay = deadline - time.perf_counter()
+    if delay > 0:
+        time.sleep(delay)
